f6ff9169e232a0ac2abda48b6de4ea8229fddb3f
morsik <morsik@darkserver.it>
2017-03-25 13:50:16 +0100

    Add option for enabling htaccess

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,14 @@
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
 
+# enable htaccess
+if [ "${HTTPD_HTACCESS}" ]; then
+cat > /etc/httpd/conf.d/99-httpd-htaccess.conf << EOF
+<Directory "/var/www/html">
+    AllowOverride All
+</Directory>" > /etc/httpd/conf.d/99-httpd-htaccess.conf
+EOF
+fi
+
 exec /usr/sbin/apachectl -DFOREGROUND
 
